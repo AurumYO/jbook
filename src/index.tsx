4afbd272d6c35d6a669b907cde2fd1e3cc2896fc
Yuri Osadchyi <aurum.yo@gmail.com>
2024-10-09 21:20:20 +0300

    Safely Handle Untrusted Code Execution;

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,8 @@ const root = ReactDOM.createRoot(el!);
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -28,6 +28,8 @@ const App = () => {
       return;
     }
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -39,10 +41,29 @@ const App = () => {
       },
     });
 
-    // console.log(result);
-
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+  <html>
+    <head></head>
+  <body>
+    <div id="root"></div>
+      <script>
+        window.addEventListener('message', (event) => {
+          try {
+            eval(event.data);
+          } catch (err) {
+           const root = document.querySelector('#root');
+           root.innerHTML = '<div style="color: red;"><h4>RuntimeError</h4>' + err + '</div>';
+           console.error(err);
+          }
+        }, false);
+      </script>
+    </body>
+  </html>
+`;
 
   return (
     <div>
@@ -53,7 +74,7 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html}></iframe>
     </div>
   );
 };
