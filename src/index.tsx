3d852c82be304cc901faa920d37287430bbaf2a7
Yuri Osadchyi <aurum.yo@gmail.com>
2024-10-10 13:49:21 +0300

    Add Monaco Editor; Custom style editor;

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
+import CodeEditor from './components/code-editor';
 
 const el = document.getElementById("root");
 
@@ -67,6 +69,10 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor 
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+        />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
