57dbb3c7d6236b4bb2e35598e48fd5453f1182cd
Yuri Osadchyi <aurum.yo@gmail.com>
2024-10-28 16:25:20 +0200

    Add Lerna package management;

---
+++ packages/cli/src/commands/serve.ts
@@ -0,0 +1,13 @@
+import path from 'path';
+import  { Command } from 'commander';
+import { serve } from 'local-api';
+
+
+export const serveCommand = new Command()
+    .command('serve [filename]')
+    .description('Open a file for editing')
+    .option('-p, --port <number>', 'port to run server on', '4005')
+    .action((filename = 'notebook.js', options) => {
+        const dir = path.join(process.cwd(), path.dirname(filename), path.basename(filename));
+        serve(options.port, filename, dir);
+    });
